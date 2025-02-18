4d5b76c2d39a93fc1b44e27affeb4a859f183ff2
Yubeen Om <yubeen.om@gmail.com>
2025-02-18 23:08:47 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+// 유저가 값을 입력한다.
+// +버튼을 클릭하면 할일이 추가된다
+// delete 버튼을 누르면 할일이 삭제된다
+// check버튼을 누르면 할 일이 끝나면서 밑줄이 간다
+// 진행중 끝남 탭을 누르면, 언더바가 이동한다
+// 끝남탭은, 끝난 아이템만, 진행중탭은 진행중인 아이템만 나타난다
+// 전체탭을 누르면 다시 전체 아이템으로 돌아옴
+
+let taskInput = document.getElementById("task-input")
+let plusButton = document.getElementById("plus-button")
+let taskList = []
+
+plusButton.addEventListener("click",addTask)
+plusButton.addEventListener("click", function(){taskInput.value=""})
+
+function addTask(){
+    let taskContent = taskInput.value
+
+    if(taskContent==""){
+        return;
+    }
+
+    if(taskList.includes(taskContent)===true){
+        return;
+    }
+
+    taskList.push(taskContent)
+    console.log(taskList)
+    render()
+}
+
+function render(){
+    let resultHTML = ``
+    for(let i=0; i<taskList.length; i++){
+        resultHTML += `<div class="task">
+                    <div>${taskList[i]}</div>
+                    <div>
+                        <button>Start</button>
+                        <button>Done</button>
+                        <button>Delete</button>
+                    </div>
+                </div>`
+
+    }
+
+    document.getElementById("task-board").innerHTML = resultHTML
+}
+
