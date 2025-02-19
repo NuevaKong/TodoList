ba0498762c93f2dbfbb4ecd43ea8229cf741b053
Yubeen Om <yubeen.om@gmail.com>
2025-02-20 00:58:40 +0900

    second commit

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 // +버튼을 클릭하면 할일이 추가된다
 // delete 버튼을 누르면 할일이 삭제된다
 // check버튼을 누르면 할 일이 끝나면서 밑줄이 간다
+//  1. check 버튼을 클릭하는 순간 true false
+//  2. true이면 끝난걸로 간주하고 밑줄 보여주기
+//  3. false이면 안끝난걸로 간주하고 그대로
 // 진행중 끝남 탭을 누르면, 언더바가 이동한다
 // 끝남탭은, 끝난 아이템만, 진행중탭은 진행중인 아이템만 나타난다
 // 전체탭을 누르면 다시 전체 아이템으로 돌아옴
@@ -14,17 +17,13 @@ plusButton.addEventListener("click",addTask)
 plusButton.addEventListener("click", function(){taskInput.value=""})
 
 function addTask(){
-    let taskContent = taskInput.value
-
-    if(taskContent==""){
-        return;
+    let task ={
+        id: randomIDGenerate(),
+        taskContent: taskInput.value,
+        isComplete: false
     }
 
-    if(taskList.includes(taskContent)===true){
-        return;
-    }
-
-    taskList.push(taskContent)
+    taskList.push(task)
     console.log(taskList)
     render()
 }
@@ -32,17 +31,54 @@ function addTask(){
 function render(){
     let resultHTML = ``
     for(let i=0; i<taskList.length; i++){
-        resultHTML += `<div class="task">
-                    <div>${taskList[i]}</div>
-                    <div>
-                        <button>Start</button>
-                        <button>Done</button>
-                        <button>Delete</button>
+        if(taskList[i].isComplete == true){
+            resultHTML+=`<div class="task task-done">
+                    <span>${taskList[i].taskContent}</span>
+                    <div class="button-box">
+                        <button style="color: rgb(126, 126, 126);"><i class="fa-solid fa-play"></i></button>
+                        <button onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-rotate-right"></i></button>
+                        <button onclick="deleteTask('${taskList[i].id}')" style="color: red;"><i class="fa-solid fa-trash-can"></i></button>
                     </div>
                 </div>`
+        } else{
+            resultHTML += `<div class="task">
+            <span>${taskList[i].taskContent}</span>
+            <div class="button-box">
+                <button style="color: rgb(126, 126, 126);"><i class="fa-solid fa-play"></i></button>
+                <button onclick="toggleComplete('${taskList[i].id}')" style="color: rgb(0, 168, 0);;"><i class="fa-solid fa-check"></i></button>
+                <button onclick="deleteTask('${taskList[i].id}')" style="color: red;"><i class="fa-solid fa-trash-can"></i></button>
+            </div>
+        </div>`
+        }
 
     }
 
     document.getElementById("task-board").innerHTML = resultHTML
 }
 
+function toggleComplete(id){
+    console.log("id:",id)
+    for(let i=0; i<taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList[i].isComplete= !taskList[i].isComplete;
+            break;
+        }
+    }
+
+    render()
+    console.log(taskList)
+}
+
+function deleteTask(id){
+    for(let i=0; i<taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList.splice(i,1)
+            break;
+        }
+    }
+    render()
+}
+
+function randomIDGenerate(){
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
